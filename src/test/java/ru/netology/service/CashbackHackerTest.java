9ad40832685cf71e38f2c5605de27dad34ad1a3e
Yuri Kopshev <yura.kopshev@gmail.com>
2020-09-07 14:10:39 +0200

    tests changed for JUnit4

--- src/test/java/ru/netology/service/CashbackHackerTest.java
+++ src/test/java/ru/netology/service/CashbackHackerTest.java
@@ -1,31 +1,32 @@
 package ru.netology.service;
 
 
-import static org.testng.Assert.*;
+import static org.junit.Assert.*;
+
 
 public class CashbackHackerTest {
     CashbackHacker hacker = new CashbackHacker();
 
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldTestRemain() {
         int expected = 0;
         int actual = hacker.remain(1000);
-        assertEquals(actual, expected);
+        assertEquals(expected,actual);
     }  // This test failed, because function return 1000, but must 0;
 
-    @org.testng.annotations.Test
+      @org.junit.Test
     public void shouldTestRemainLow() {
         int expected = 1;
         int actual = hacker.remain(999);
-        assertEquals(actual, expected);
+        assertEquals(expected,actual);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldTestRemainHigh() {
         int expected = 999;
         int actual = hacker.remain(1001);
-        assertEquals(actual, expected);
+        assertEquals(expected,actual);
     }
 
 
